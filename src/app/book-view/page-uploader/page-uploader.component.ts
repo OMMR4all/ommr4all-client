8de41fb8b07ed1873ef94fc5812c5feff9925fc6
Christoph Wick <wick.chr.info@gmail.com>
2019-01-07 13:14:03 +0100

    Fixed file upload: no autoreset, instead resetting when queue processed

--- src/app/book-view/page-uploader/page-uploader.component.ts
+++ src/app/book-view/page-uploader/page-uploader.component.ts
@@ -1,6 +1,17 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {BookCommunication} from '../../data-types/communication';
 import {AuthenticationService} from '../../authentication/authentication.service';
+import {DropzoneComponent} from 'ngx-dropzone-wrapper';
 
 @Component({
   selector: 'app-page-uploader',
@@ -9,6 +20,8 @@ import {AuthenticationService} from '../../authentication/authentication.service
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageUploaderComponent implements OnInit {
+  @ViewChild(DropzoneComponent) componentRef?: DropzoneComponent;
+
   private _book: BookCommunication;
   @Input() set book(book: BookCommunication) {
     if (this._book !== book) {
@@ -33,26 +46,42 @@ export class PageUploaderComponent implements OnInit {
   ngOnInit() {
   }
 
-  onUploadError(event) {
-    console.log(event);
+  onSingleUploadError(event) {
   }
 
-  onUploadSuccess(event) {
-    console.log(event);
+  onSingleUploadSuccess(event) {
+  }
+
+  onSingleCanceled(event) {
+  }
+
+  onSingleComplete(event) {
+  }
+
+  onQueueComplete(event) {
+    this.componentRef.directiveRef.reset(false);
+  }
+
+  onReset(event) {
     this.uploadSuccess.emit();
+  }
+
+  onMaxFilesReached(event) {
+  }
+
+  onMaxFilesExceeded(event) {
   }
 
   private updateConfig() {
     if (!this.book || !this.auth.token) { return {}; }
     this._config = {
       url: '/api/book/' + this.book.book + '/upload/',
-      maxFilesize: 50,
+      maxFilesize: 500,
       acceptedFiles: 'image/*',
       headers: {
         'Authorization': 'JWT ' + this.auth.token,
       },
       autoProcessQueue: true,
-      autoReset: 500,
     };
   }
 
