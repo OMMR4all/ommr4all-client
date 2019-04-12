14c8b3fca40718c22b86f31ab3e95f7623cb6d74
Christoph Wick <wick.chr.info@gmail.com>
2019-04-12 15:11:43 +0200

    Syllable property widget can change drop capital and all word connections

--- src/app/editor/property-widgets/syllable-property-widget/syllable-property-widget.component.ts
+++ src/app/editor/property-widgets/syllable-property-widget/syllable-property-widget.component.ts
@@ -19,6 +19,8 @@ import {ActionsService} from '../../actions/actions.service';
 import {ActionType} from '../../actions/action-types';
 import {CommandChangeProperty} from '../../undo/util-commands';
 import {SyllableConnector} from '../../../data-types/page/annotations';
+import ChangeEvent = JQuery.ChangeEvent;
+import {Sentence} from '../../../data-types/page/sentence';
 
 @Component({
   selector: 'app-syllable-property-widget',
@@ -27,6 +29,7 @@ import {SyllableConnector} from '../../../data-types/page/annotations';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SyllablePropertyWidgetComponent implements OnInit, DoCheck {
+  SCT = SyllableConnectionType;
   private _prevSyllable = new Syllable();
   @Input() syllable: Syllable = null;
   @Input() selectedSyllableConnection: SyllableConnector = null;
@@ -35,6 +38,18 @@ export class SyllablePropertyWidgetComponent implements OnInit, DoCheck {
   @Output() syllableClicked = new EventEmitter<SyllableClickEvent>();
 
   @ViewChild('textInput') textElem: ElementRef;
+
+  getPrevPageLine() {
+    const pageLine = this.page.readingOrder.readingOrder.filter(pl => pl.sentence.hasSyllable(this.syllable))[0];
+    if (!pageLine) { return false; }  // must not happen
+    return this.page.readingOrder.readingOrder[this.page.readingOrder.readingOrder.indexOf(pageLine) - 1];
+  }
+
+  hasDropCapital() {
+    if (this.syllable.prefix.length > 0) { return true; }
+    const prev = this.getPrevPageLine();
+    return prev && prev.blockType === BlockType.DropCapital;
+  }
 
   get text() { return this.syllable.text; }
   set text(t: string) {
@@ -46,17 +61,25 @@ export class SyllablePropertyWidgetComponent implements OnInit, DoCheck {
     }
   }
 
-  get showVisibleDash() { return this.syllable.connection !== SyllableConnectionType.New; }
-  get visibleDash() { return this.syllable.connection === SyllableConnectionType.Visible; }
-  set visibleDash(b: boolean) {
-    if (this.showVisibleDash) {
+  get prefix() { return this.syllable.prefix; }
+  set prefix(t: string) {
+    if (this.prefix !== t) {
+      const prev = this.getPrevPageLine();
+      if (!prev || prev.blockType !== BlockType.DropCapital) { return; }
       this.actions.startAction(ActionType.LyricsEdit, [this.syllable]);
-      this.actions.run(new CommandChangeProperty(this.syllable, 'connection', this.syllable.connection,
-        b ? SyllableConnectionType.Visible : SyllableConnectionType.Hidden,
-      ));
+      this.actions.changeLyrics(prev, new Sentence([new Syllable(t)]));
+      this.actions.run(new CommandChangeProperty(this.syllable, 'prefix', this.syllable.prefix, t));
       this.actions.finishAction();
       this.syllableChanged.emit();
     }
+  }
+
+  get syllableConnection() { return this.syllable.connection; }
+  set syllableConnection(sct: SyllableConnectionType) {
+    this.actions.startAction(ActionType.LyricsEdit, [this.syllable]);
+    this.actions.run(new CommandChangeProperty(this.syllable, 'connection', this.syllable.connection, sct));
+    this.actions.finishAction();
+    this.syllableChanged.emit();
   }
 
   constructor(
@@ -83,9 +106,9 @@ export class SyllablePropertyWidgetComponent implements OnInit, DoCheck {
     return this.page.readingOrder.readingOrder.filter(p => p.blockType === BlockType.Lyrics);
   }
 
-  textChanged() {
-    this.text = this.textElem.nativeElement.value.replace(/[ \-~]/, '');
-    this.textElem.nativeElement.value = this.text;
+  textChanged(event: Event) {
+    const elem = event.target as HTMLInputElement;
+    elem.value = elem.value.replace(/[ \-~]/, '');
     this.changeDetector.markForCheck();
   }
 }
