6470f6c7c90e22f5ae91fe01a62451093a7a2b23
C. Wick <wick.chr.info@gmail.com>
2019-05-05 19:26:53 +0200

    Redirecting to login page also if json web token expired

--- src/app/authentication/error-inceptor.ts
+++ src/app/authentication/error-inceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {AuthenticationService} from './authentication.service';
@@ -13,10 +13,16 @@ export class ErrorInterceptor implements HttpInterceptor {
     ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(catchError(err => {
+    return next.handle(request).pipe(catchError((err: HttpErrorResponse) => {
       if (err.status === 401) {
-        if (this.authenticationService.isLoggedOut()) {
-          // if logged out, go to login page
+        // either if logged out or the json token is expired redirect to login page (note this matches on the error message and might be
+        // adjusted if the django rest api for json token changed
+        if (this.authenticationService.isLoggedOut() || (err.error && err.error.detail && err.error.detail.indexOf('expired') >= 0)) {
+          if (this.authenticationService.isLoggedIn()) {
+            // if logged in and an error accurred, log out automatically
+            this.authenticationService.logout();
+          }
+          // redirect to login page
           this.router.navigate(['/login'], { queryParams: { redirect: this.router.url.split('?')[0] }});
         }
       }
